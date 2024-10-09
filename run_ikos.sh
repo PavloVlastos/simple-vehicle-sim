911b2cc7efe62bcbef38debcf4d98bdfc2cc1b47
Pavlo <pgv2station@gmail.com>
2024-10-09 10:47:49 -0700

    temporary fix for sim module paths for make with ikos

--- run_ikos.sh
+++ run_ikos.sh
@@ -8,10 +8,12 @@ ikos -I modules/common/ \
     -I modules/interface \
     -I modules/model \
     -I modules/plan \
-    svs_sim_main.c -o ikos_generated/svs_sim_main.db
+    -I modules/map \
+    -I modules/planner \
+    main.c -o ikos_generated/main.db
     # -a=boa,dbz,nullity,prover,upa,uva,sio,uio,shc,poa,pcmp,sound,fca,dca,dfa,dbg,watch\
 
-ikos -I modules/common/ \
-     -I modules/lin_alg \
-     --entry-points=main,sum_vec\
-    UT_lin_alg/UT_lin_alg.c -o ikos_generated/UT_lin_alg.db
+# ikos -I modules/common/ \
+#      -I modules/lin_alg \
+#      --entry-points=main,sum_vec\
+#     UT_lin_alg/UT_lin_alg.c -o ikos_generated/UT_lin_alg.db
